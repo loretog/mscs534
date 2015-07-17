7d4216e2f90f43c88f340066b70ed480ff2184e1
loreto <jun.rej@gmail.com>
2015-07-18 03:50:49 +0800

    4th

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
   $('#fullpage').fullpage({
   	menu: '#menu',  	
-  	anchors:['about-us', 'what-we-do', 'our-team', 'testimonials']
+  	anchors:['about-us', 'what-we-do', 'our-team', 'signup' ],
+  	sectionsColor : [ '#fff', '#FCD54C', '#ffffff', '#ffffff' ]  	
   });
 });
