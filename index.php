452e6221d7236f4edd8ed698a75a95ec38db05fe
loreto <jun.rej@gmail.com>
2015-07-18 01:11:49 +0800

    second commit

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+	/*
+	 *
+	 * 	Site Name: 		Best Layer, Inc
+	 * 	Description: 	Case study for MSCS534
+	 * 	Author: 			Loreto Gabawa Jr.
+	 *	Site: 				www.tagapagkodigo.com	
+	 *	GitHub:				https://github.com/loretog/mscs534
+	 *
+   */
+?> 
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="./assets/bootstrap/css/bootstrap.min.css">
+</head>
+<body>
+
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-lg-12">
+				<div class="top-heading">
+					<div class="menu">
+						<ul>
+							<li><a href="./">Home</a></li>
+							<li><a href="./">Home</a></li>
+							<li><a href="./">Home</a></li>
+							<li><a href="./">Home</a></li>
+							<li><a href="./">Home</a></li>
+						</ul>
+					</div>					
+				</div> <!-- /.top-heading -->
+			</div>
+		</div>
+	</div>
+
+	<script type="text/javascript" src="./assets/js/jquery-2.1.4.min.js"></script>
+	<script type="text/javascript" src="./assets/bootstrap/js/bootsrap.min.js"></script>
+</body>
+</html>
